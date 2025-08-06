1dc3da4add2d29413dc4fd4e3ec66a7a3870dd12
Luciajantus <luciajantus@gmail.com>
2025-08-06 10:17:33 -0300

    Descripción del cambio

--- 2035-logica-programacion-2-ProyectoInicial/app.js
+++ 2035-logica-programacion-2-ProyectoInicial/app.js
@@ -1,1 +1,11 @@
-document.querySelector(`h1`);
+let titulo = document.querySelector(`h1`);
+titulo.innerHTML = `Juego del número secreto`;
+
+let parrafo = document.querySelector(`p`);
+parrafo.innerHTML = `Adivina el número entre 1 y 10`;
+
+
+function intentoDeUsuario() {   
+    alert("click desde el botón");
+
+}
